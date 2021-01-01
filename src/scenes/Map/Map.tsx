df1c47a5ed9dc57252dd62e0dc1d91092868a976
Radek Palisa <radek.palisa@gmail.com>
2021-01-01 16:19:34 +0100

    useMap hook

--- src/scenes/Map/Map.tsx
+++ src/scenes/Map/Map.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
 import './Map.css';
-import { Loader } from '@googlemaps/js-api-loader';
 import onLongPress from './services/onLongPress';
 import ActionPanel from './components/ActionPanel/ActionalPanel';
 import Button from './components/Button/Button';
@@ -12,157 +11,134 @@ import Typography from './components/Typography/Typography';
 import getBikeStationIcon from './services/utils/getBikeStationIcon';
 
 import dotStation from '../../assets/dot-station.svg';
-
-const loader = new Loader({
-  apiKey: process.env.REACT_APP_GOOGLE_API_KEY || '',
-  version: 'weekly',
-  libraries: ['geometry'],
-});
+import useMap from './services/useMap';
 
 export default function Map() {
-  const mapDiv = useRef<HTMLDivElement>(null);
+  const mapDivRef = useRef<HTMLDivElement>(null);
   const marker = useRef<google.maps.Marker | null>(null);
   const centerMapToCurrentPosition = useRef(() => {});
   const [destination, setDestination] = useState<google.maps.LatLng | null>(null);
+  const map = useMap(mapDivRef);
 
   useEffect(() => {
-    loader.load().then(() => {
-      const map = new google.maps.Map(mapDiv.current as HTMLDivElement, {
-        center: { lat: 41.4013398, lng: 2.2028568 },
-        zoom: 15,
-        disableDefaultUI: true,
-        // mapTypeControl: false,
-        // streetViewControl: false,
-        // zoomControl: false,
-        // fullscreenControl: false,
-        // rotateControl: true,
-        // scaleControl: false,
-        gestureHandling: 'greedy',
+    if (!map) return;
+
+    onLongPress(map, e => {
+      if (marker.current) {
+        marker.current.setMap(null);
+      }
+
+      marker.current = new google.maps.Marker({
+        position: e.latLng,
+        map: map,
       });
 
-      onLongPress(map, e => {
-        if (marker.current) {
-          marker.current.setMap(null);
-        }
+      setDestination(e.latLng);
+    });
 
-        marker.current = new google.maps.Marker({
-          position: e.latLng,
-          map: map,
-        });
+    map.addListener('click', () => {
+      if (marker.current) {
+        marker.current.setMap(null);
+        setDestination(null);
+      }
+    });
 
-        setDestination(e.latLng);
-      });
+    const currentPositionControl = new CurrentPositionControl(map);
+    const currentPositionPromise = currentPositionControl.getCurrentPosition();
 
-      map.addListener('click', () => {
-        if (marker.current) {
-          marker.current.setMap(null);
-          setDestination(null);
-        }
-      });
+    centerMapToCurrentPosition.current = currentPositionControl.centerMapToCurrentPosition;
 
-      const currentPositionControl = new CurrentPositionControl(map);
-      const currentPositionPromise = currentPositionControl.getCurrentPosition();
+    const stationsInfoPromise = getBicingStationsGeoData().then(data => {
+      map.data.addGeoJson(data);
+      return data;
+    });
 
-      centerMapToCurrentPosition.current = currentPositionControl.centerMapToCurrentPosition;
+    map.data.setStyle(feature => {
+      if (feature.getProperty('shouldShowStatus')) {
+        const availableMechanical = feature.getProperty('availableMechanical') || 0;
+        const availableEbike = feature.getProperty('availableElectric') || 0;
+        const capacity = feature.getProperty('capacity') || 0;
 
-      const stationsInfoPromise = getBicingStationsGeoData().then(data => {
-        map.data.addGeoJson(data);
-        return data;
-      });
+        const dec = (availableMechanical + availableEbike) / capacity;
 
-      map.data.setStyle(feature => {
-        if (feature.getProperty('shouldShowStatus')) {
-          const availableMechanical = feature.getProperty('availableMechanical') || 0;
-          const availableEbike = feature.getProperty('availableElectric') || 0;
-          const capacity = feature.getProperty('capacity') || 0;
+        // rounded to 1 decimal but never less than 0.1 or more than 1
+        const availableTotal = Math.min(
+          Math.max(Math.round((dec + Number.EPSILON) * 10) / 10, 0.1),
+          1
+        );
 
-          const dec = (availableMechanical + availableEbike) / capacity;
-
-          // rounded to 1 decimal but never less than 0.1 or more than 1
-          const availableTotal = Math.min(
-            Math.max(Math.round((dec + Number.EPSILON) * 10) / 10, 0.1),
-            1
-          );
-
-          return {
-            icon: {
-              url:
-                'data:image/svg+xml;charset=UTF-8,' +
-                encodeURIComponent(getBikeStationIcon(availableTotal)),
-            },
-          };
-        }
         return {
           icon: {
-            url: dotStation,
+            url:
+              'data:image/svg+xml;charset=UTF-8,' +
+              encodeURIComponent(getBikeStationIcon(availableTotal)),
           },
         };
-      });
-
-      const nearStationsPromise: Promise<GeoJsonFeature[]> = Promise.all([
-        stationsInfoPromise,
-        currentPositionPromise,
-      ]).then(([stationsInfoGeoJson, currentPosition]) => {
-        const nearStations: Array<GeoJsonFeature> = [];
-
-        if (!currentPosition) {
-          return nearStations; // empty
-        }
-
-        stationsInfoGeoJson.features.forEach(feature => {
-          const { geometry } = feature;
-          const to = new google.maps.LatLng({
-            lng: geometry.coordinates[0],
-            lat: geometry.coordinates[1],
-          });
-          const distance = google.maps.geometry.spherical.computeDistanceBetween(
-            currentPosition,
-            to
-          );
-          // meters
-          if (distance < 600) {
-            nearStations.push(feature);
-          }
-        });
-
-        return nearStations;
-      });
-
-      const stationsStatusesPromise = fetchBicingStationsStatus();
-
-      Promise.all([nearStationsPromise, stationsStatusesPromise]).then(
-        ([nearStations, stationStatuses]) => {
-          const nearStationsCopy = [...nearStations];
-
-          stationStatuses.data.stations.forEach(station => {
-            const feature = map.data.getFeatureById(station.station_id);
-
-            if (!feature) return;
-
-            if (nearStationsCopy.length) {
-              const nearStationIndex = nearStationsCopy?.findIndex(s => s.id === feature.getId());
-
-              if (nearStationIndex > -1) {
-                nearStationsCopy.splice(nearStationIndex, 1);
-                feature.setProperty('shouldShowStatus', true);
-              }
-            }
-
-            feature.setProperty(
-              'availableMechanical',
-              station.num_bikes_available_types.mechanical
-            );
-            feature.setProperty('availableElectric', station.num_bikes_available_types.ebike);
-            feature.setProperty('availableDocks', station.num_docks_available);
-          });
-        }
-      );
+      }
+      return {
+        icon: {
+          url: dotStation,
+        },
+      };
     });
-  }, []);
+
+    const nearStationsPromise: Promise<GeoJsonFeature[]> = Promise.all([
+      stationsInfoPromise,
+      currentPositionPromise,
+    ]).then(([stationsInfoGeoJson, currentPosition]) => {
+      const nearStations: Array<GeoJsonFeature> = [];
+
+      if (!currentPosition) {
+        return nearStations; // empty
+      }
+
+      stationsInfoGeoJson.features.forEach(feature => {
+        const { geometry } = feature;
+        const to = new google.maps.LatLng({
+          lng: geometry.coordinates[0],
+          lat: geometry.coordinates[1],
+        });
+        const distance = google.maps.geometry.spherical.computeDistanceBetween(currentPosition, to);
+        // meters
+        if (distance < 600) {
+          nearStations.push(feature);
+        }
+      });
+
+      return nearStations;
+    });
+
+    const stationsStatusesPromise = fetchBicingStationsStatus();
+
+    Promise.all([nearStationsPromise, stationsStatusesPromise]).then(
+      ([nearStations, stationStatuses]) => {
+        const nearStationsCopy = [...nearStations];
+
+        stationStatuses.data.stations.forEach(station => {
+          const feature = map.data.getFeatureById(station.station_id);
+
+          if (!feature) return;
+
+          if (nearStationsCopy.length) {
+            const nearStationIndex = nearStationsCopy?.findIndex(s => s.id === feature.getId());
+
+            if (nearStationIndex > -1) {
+              nearStationsCopy.splice(nearStationIndex, 1);
+              feature.setProperty('shouldShowStatus', true);
+            }
+          }
+
+          feature.setProperty('availableMechanical', station.num_bikes_available_types.mechanical);
+          feature.setProperty('availableElectric', station.num_bikes_available_types.ebike);
+          feature.setProperty('availableDocks', station.num_docks_available);
+        });
+      }
+    );
+  }, [map]);
 
   return (
     <div id="map-container">
-      <div id="map" ref={mapDiv}></div>
+      <div id="map" ref={mapDivRef}></div>
       <ActionPanel isOn={Boolean(destination)}>
         <Typography gutterBottom>Dropped pin</Typography>
         <Typography color="textSecondary" paragraph>
