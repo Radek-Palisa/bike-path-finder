8e6d72876b5a329e4806c7b975c1547f0bc1f6bf
Mila Sevilla <m.sevillazhou@gmail.com>
2021-01-03 12:39:51 +0100

    disable mui ripple effect on findMyLocation btn

--- src/scenes/Map/components/Buttons/FindMyLocationBtn.tsx
+++ src/scenes/Map/components/Buttons/FindMyLocationBtn.tsx
@@ -14,6 +14,7 @@ export default function FindMyLocationBtn({ onClick }: Props) {
   return (
     <Fab
       id="fab-findLocationButton"
+      disableRipple
       onClick={() => {
         window.clearTimeout(timeoutId.current);
         onClick();
